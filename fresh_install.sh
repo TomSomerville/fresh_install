8bd31afb7d2c6834b2153b7cfde19bd83a8322a4
Thomas Somerville <thoams.somerv@gmail.com>
2023-05-24 16:28:02 -0400

    Rev 1 Done

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,11 +1,50 @@
 #!/bin/sh
-echo "Running apt update..."
+echo "\n##########\nRunning apt update...\n##########\n"
 apt update
 
-echo "Running apt upgrade..."
+echo "\n##########\nRunning apt upgrade...\n##########\n"
 apt upgrade -y
 
-echo "Running apt git install..."
-apt git install -y
+echo "\n##########\nRunning apt install git...\n##########\n"
+apt install git -y
+
+echo "Running apt install curl..."
+apt install curl -y
+
+echo "\n##########\nRunning apt install flatpak...\n##########\n"
+apt install flatpak -y
+
+echo "\n##########\nSetting up flatpak\n##########\n"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "\n##########\nRunning apt install vim...\n##########\n"
+apt install vim -y
+
+echo "Setting up VIM..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vimrc ~/.vimrc
+
+echo "Running apt install python3-pip..."
+apt install python3-pip -y
+
+echo "Setting up python..."
+python3 -m pip install --user virtualenv
+
+echo "Running apt install nmap..."
+apt install nmap -y
+
+echo "Installing Flathub applications..."
+flatpak install flathub com.brave.Browser -y
+flatpak install flathub com.slack.Slack -y
+flatpak install flathub org.mozilla.firefox -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.sublimetext.three -y
+flatpak install flathub org.flameshot.Flameshot -y
+flatpak install flathub com.github.debauchee.barrier -y
+flatpak install flathub org.sqlitebrowser.sqlitebrowser -y
+flatpak install flathub org.ghidra_sre.Ghidra -y
+flatpak install flathub io.github.Hexchat -y
+flatpak install flathub io.github.java_decompiler.jd-gui -y
+
 
 
---
+++ vimrc
@@ -0,0 +1,33 @@
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin()
+" The default plugin directory will be as follows:
+"   - Vim (Linux/macOS): '~/.vim/plugged'
+"   - Vim (Windows): '~/vimfiles/plugged'
+"   - Neovim (Linux/macOS/Windows): stdpath('data') . '/plugged'
+" You can specify a custom plugin directory by passing it as the argument
+"   - e.g. `call plug#begin('~/.vim/plugged')`
+"   - Avoid using standard Vim directory names like 'plugin'
+
+
+Plug 'preservim/nerdtree'
+Plug 'vim-airline/vim-airline'
+Plug 'Shougo/deoplete.nvim'
+Plug 'jiangmiao/auto-pairs'
+Plug 'python-mode/python-mode'
+
+" Make sure you use single quotes
+"  Initialize plugin system
+" - Automatically executes `filetype plugin indent on` and `syntax enable`.
+call plug#end()
+" You can revert the settings after the call like so:
+"   filetype indent off   " Disable file-type-specific indentation
+"   syntax off            " Disable syntax highlighting
+"
+set number
+set title
+
