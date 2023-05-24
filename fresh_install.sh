1d3e23412fe97c7dae947311922922fc9f094e78
Thomas Somerville <thoams.somerv@gmail.com>
2023-05-24 13:41:16 -0400

    edit 1

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-echo "Running `apt update`..."
+echo "Running apt update..."
 apt update
 
-echo "Running `apt upgrade`..."
+echo "Running apt upgrade..."
+apt upgrade -y
+
+echo "Running apt git install..."
+apt git install -y
+
+
